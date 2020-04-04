4e391a0d8b189ead5f58db03a6d526d1935fc9ea
Conor Quigley <conor.quigley144@gmail.com>
2020-04-04 18:43:02 -0400

    modify citibike aggregation

--- citibike-pyspark.py
+++ citibike-pyspark.py
@@ -1,28 +1,30 @@
-
-"""
-Start-up command
-pyspark --num-executors 16 --driver-memory 4g --executor-memory 8g --packages graphframes:graphframes:0.1.0-spark1.6 < script.py
-"""
-
-
-from graphframes import *
+#Import necessary libraries
+#from graphframes import *
 from functools import reduce
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit, when
 
 
-df = spark.read.csv("/user/conorquigley/citibike/*",header=True)
-df.cache()
+if __name__ == "__main__":
+    #build an SparkSession using the SparkSession APIs
+    spark = (SparkSession
+           .builder
+           .appName("CitiBike")
+           .getOrCreate())
 
-df_stations = 
-        df.select(
-            col("start station id").alias("station_id"),
-            col("start station name").alias("station_name")
-        )
-        .union(
+    #read CitiBike data from HDFS into
+    df = spark.read.csv("/user/conorquigley/citibike/*", header = True)
+    df.cache()
+    df_stations = 
             df.select(
-                "end station id",
-                "end station name")
+                col("start station id").alias("station_id"),
+                col("start station name").alias("station_name")
             )
-        .distinct()
+            .union(
+                df.select(
+                    "end station id",
+                    "end station name")
+                )
+            .distinct()
 
-df_stations.groupBy("station_id").count().sort("count",ascending=False).show(25)
+    df_stations.groupBy("station_id").count().sort("count", ascending = False).show(25)
