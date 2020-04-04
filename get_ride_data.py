fdcd380ccd2c76f090939a553a976922430ebab3
Conor Quigley <conor.quigley144@gmail.com>
2020-04-03 20:26:54 -0400

    completed get_ride_data.py

--- get_ride_data.py
+++ get_ride_data.py
@@ -1,25 +1,32 @@
 import boto3
 import subprocess
+import logging
 
+#Initialize s3 bucket
+bucket_name = 'tripdata'
 s3 = boto3.resource('s3')
-my_bucket = s3.Bucket('tripdata')
+trip_bucket = s3.Bucket(bucket_name)
+
+#Define target directory
+trip_hdfs = '/user/clsadmin/data'
 
 def get_s3_ride_files(trip_bucket) -> list:
     """
     Return list of ride files stored in CitiBike s3
     """
     all_files = []
-    for my_bucket_object in my_bucket.objects.all():
-        all_files.append(my_bucket_object.key)
-    ride_files = [x for x in all_files if x[:2]=="20" and x.count("-")<3]
+    for file in trip_bucket.objects.all():
+        all_files.append(file.key)
+    ride_files = [x for x in all_files if x[:2]=='20' and x.count('-')<3]
     return ride_files
 
 def get_hdfs_ride_files(hdfs_dir) -> list:
     """
     Return list of ride files already stored in HDFS
     """
-
-
+    output = subprocess.check_output(f'hdfs dfs -ls {hdfs_dir}', shell=True)
+    files = str(output).split(hdfs_dir)[1].strip('/').split(r'\\n')[0]
+    return files
 
 def find_file_diff(s3_files, hdfs_files) -> list:
     """
@@ -35,3 +42,32 @@ def copy_ride_files_s3_2_hdfs(trip_bucket, hdfs_dir, ride_files):
     """
     Copy files from s3 to hdfs directory
     """
+    for file in ride files:
+        subprocess.check_output(f'hdfs dfs -cp https://s3.amazonaws.com/{trip_bucket}/{file} {hdfs_dir}')
+
+def main():
+    try:
+        logger.info('Finding s3 files...')
+        s3_files = get_s3_ride_files(trip_bucket)
+    except Exception:
+        logger.error('Could not read s3 files')
+        sys.exit(-1)
+
+    try:
+        logger.info('Finding files already in HDFS...')
+        hdfs_files = get_hdfs_ride_files(trip_hdfs)
+    except Exception:
+        logger.error('Could not read HDFS files')
+        sys.exit(-1)
+
+    try: 
+        logger.info('Retrieving missing files...')
+        files_2_copy = find_file_diff(s3_files, hdfs_files)
+        copy_ride_files_s3_2_hdfs(bucket_name, trip_hdfs, hdfs_files)
+    except Exception:
+        logger.error('Unable to retrieve latest ride files')
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
+    
