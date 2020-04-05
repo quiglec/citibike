fdb1ad35ebc80d6db2c629fd4b4d8ab6795b20c5
Conor Quigley <conor.quigley144@gmail.com>
2020-04-05 14:51:38 -0400

    Modifying s3 retrieval

--- get_ride_data.py
+++ get_ride_data.py
@@ -18,7 +18,7 @@ URL = "https://s3.amazonaws.com/tripdata/"
 #Define target directory
 trip_hdfs = '/user/clsadmin/data'
 
-def get_s3_ride_files(url) -> list:
+def get_s3_ride_files(url: str) -> list:
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, 'html5lib')
     all_files = [key.text for key in soup.findAll('key')]
@@ -37,7 +37,7 @@ def get_s3_ride_files(trip_bucket) -> list:
     return ride_files
 """
 
-def get_hdfs_ride_files(hdfs_dir) -> list:
+def get_hdfs_ride_files(hdfs_dir: str) -> list:
     """
     Return list of ride files already stored in HDFS
     """
@@ -45,7 +45,7 @@ def get_hdfs_ride_files(hdfs_dir) -> list:
     files = str(output).split(hdfs_dir)[1].strip('/').split(r'\\n')[0]
     return files
 
-def find_file_diff(s3_files, hdfs_files) -> list:
+def find_file_diff(s3_files: list, hdfs_files: list) -> list:
     """
     Return s3 files not present in HDFS
     """
@@ -55,12 +55,12 @@ def find_file_diff(s3_files, hdfs_files) -> list:
             diff.append(file)
     return diff
 
-def copy_ride_files_s3_2_hdfs(trip_bucket, hdfs_dir, ride_files):
+def copy_ride_files_s3_2_hdfs(url: str, hdfs_dir, ride_files):
     """
     Copy files from s3 to hdfs directory
     """
     for file in ride_files:
-        subprocess.check_output(f'hdfs dfs -cp https://s3.amazonaws.com/{trip_bucket}/{file} {hdfs_dir}')
+        subprocess.check_output(f'hdfs dfs -cp {url}/{file} {hdfs_dir}')
 
 def main():
     try:
@@ -80,7 +80,7 @@ def main():
     try: 
         logger.info('Retrieving missing files...')
         files_2_copy = find_file_diff(s3_files, hdfs_files)
-        copy_ride_files_s3_2_hdfs(bucket_name, trip_hdfs, hdfs_files)
+        copy_ride_files_s3_2_hdfs(URL, trip_hdfs, hdfs_files)
     except Exception:
         logger.error('Unable to retrieve latest ride files')
         sys.exit(-1)
