8a9b84f85160fd9b3c7e6beb21d3eee4420c9130
Conor Quigley <conor.quigley144@gmail.com>
2020-04-09 01:06:00 -0400

    Modifying HDFS data read into data frame

--- citibike-pyspark.py
+++ citibike-pyspark.py
@@ -1,9 +1,34 @@
 #Import necessary libraries
-#from graphframes import *
-from functools import reduce
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, when
+from pyspark.sql.types import TimestampType, StringType
+from pyspark.sql.functions import udf, unix_timestamp, col
 
+#Define dataframe schema
+schema = """
+        `trip_duration` INT, 
+        `start_time` STRING, 
+        `stop_time` STRING, 
+        `start_station_id` STRING, 
+        `start_station_name` STRING, 
+        `start_station_latitude` FLOAT, 
+        `start_station_longitude` FLOAT, 
+        `end_station_id` STRING, 
+        `end_station_name` STRING, 
+        `end_station_latitude` FLOAT, 
+        `end_station_longitude` FLOAT, 
+        `bike_id` STRING, 
+        `user_type` STRING, 
+        `birth_year` INT, 
+        `gender` STRING
+        """
+
+#Define function for standardizing timestamp fields
+def replace_date(string: str) -> str:
+    it = string.replace("/", "-")
+    if it[:3] != "201":
+        return it.split("-")[2][:4]+"-"+it.split("-")[0]+"-"+it.split("-")[1]+it.split("-")[2][4:]
+
+replace = udf(lambda x: replace_date(x), StringType())
 
 if __name__ == "__main__":
     #build an SparkSession using the SparkSession APIs
@@ -13,7 +38,13 @@ if __name__ == "__main__":
            .getOrCreate())
 
     #read CitiBike data from HDFS into
-    df = spark.read.csv("/user/conorquigley/citibike/*", header = True)
+    df = spark.read.csv("/user/clsadmin/data/*", header = True, schema = schema)
+
+    df1 = 
+df.withColumn("start_time", unix_timestamp(replace(df.start_time),"YYYY-MM-DD HH:MM:SS").cast(TimestampType())).withColumn("stop_time", unix_timestamp(replace(df.stop_time),"YYYY-MM-DD HH:MM:SS").cast(TimestampType()))
+
+
+    """
     df.cache()
     df_stations = 
             df.select(
@@ -28,3 +59,5 @@ if __name__ == "__main__":
             .distinct()
 
     df_stations.groupBy("station_id").count().sort("count", ascending = False).show(25)
+    """
+
